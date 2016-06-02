144aa59d7b0eb9b6bb9edde1aa0d562bd2784e42
jlobos <jlobos@outlook.cl>
2016-06-02 09:12:03 -0400

    script of screen

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
-xrandr --auto
-xrandr --output LVDS1 --off
-xrandr --output HDMI1 --left-of VGA1
 
-xrandr --output VGA1 --mode 1440x900
-xrandr --output HDMI1 --mode 1280x1024
+if (xrandr | grep "VGA1 connected" > /dev/null)
+then
+  exec
+xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1440x132 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1440x900 --pos 0x0 --rotate normal
+  exit
+else
+  exec xrandr --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
+  exit
+fi
